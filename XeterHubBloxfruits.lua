b465f48ff19c8f6b64fb16b5edebfbc08afa06d3
defnot67kid-beep <defnot67kid@gmail.com>
2026-01-04 12:00:21 +0000

    Create XeterHubBloxfruits.lua

---
+++ XeterHubBloxfruits.lua
@@ -0,0 +1,16 @@
+-- Xeter V1
+getgenv().Version = "V1"
+getgenv().Team = "Marines"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TlDinhKhoi/Xeter/refs/heads/main/Main.lua"))()
+-- Xeter V2
+getgenv().Version = "V2"
+getgenv().Team = "Marines"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TlDinhKhoi/Xeter/refs/heads/main/Main.lua"))()
+-- Xeter V3
+getgenv().Version = "V3"
+getgenv().Team = "Marines"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TlDinhKhoi/Xeter/refs/heads/main/Main.lua"))()
+-- Xeter V4
+getgenv().Version = "V4"
+getgenv().Team = "Marines"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TlDinhKhoi/Xeter/refs/heads/main/Main.lua"))()
